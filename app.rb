a5f60d3503b9d7b35a70d394ccecef7c354af387
xavierloos <xavier.loos04@gmail.com>
2020-12-09 10:36:23 +0000

    Getting the data from the from

--- app.rb
+++ app.rb
@@ -4,4 +4,10 @@ class Birthday < Sinatra::Base
   get "/" do
     erb :index
   end
+  post "/birthday" do
+    @name = params[:name]
+    @day = params[:day]
+    @month = params[:month]
+    erb :birthday
+  end
 end

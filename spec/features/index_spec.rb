f3baa0c23c2789bdbb06e509defa223fc6d60c26
xavierloos <xavier.loos04@gmail.com>
2020-12-09 13:42:13 +0000

    Filling the form with RSPEC

--- spec/features/index_spec.rb
+++ spec/features/index_spec.rb
@@ -6,16 +6,30 @@ feature "index" do
     visit "/"
     expect(page).to have_content "Hello there!"
   end
-  scenario "has a form to fill" do
-    visit "/"
-    expect(page).to have_tag "form"
-  end
   scenario "asks for a name" do
     visit "/"
     expect(page).to have_content "What's your name?"
   end
+  scenario "fills the name" do
+    visit "/"
+    fill_in("name", with: "Javi")
+    click_on "Submit"
+    expect(page).to have_content "Javi"
+  end
   scenario "asks birthday" do
     visit "/"
     expect(page).to have_content "When's your birthday?"
+  end
+  scenario "fills the day" do
+    visit "/"
+    fill_in("day", with: 4)
+    click_on "Submit"
+    expect(page).to have_content 4
+  end
+  scenario "fills the month" do
+    visit "/"
+    fill_in("month", with: "Oct")
+    click_on "Submit"
+    expect(page).to have_content "Oct"
   end
 end
